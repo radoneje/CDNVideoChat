815c3c227da8f806ff163eac6c2f4eb26feb3b9e
radoneje <den.shevchenko@gmail.com>
2022-07-24 01:06:49 +0300

    e

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -19,8 +19,8 @@ router.post('/room', async (req, res, next) =>{
 router.get('/room/:skip?', async (req, res, next) =>{
   if(!req.session.admin)
     return res.status(401)
-  if(!r.params.skip)
-    r.params.skip=0;
+  if(!req.params.skip)
+    req.params.skip=0;
   let r= await req.knex.select("*").from("t_rooms").where({isDeleted:null}).orderBy("dateCreated","desc").limit(50).offset(Number(r.params.skip)|| 0)
   let total= await req.knex.count('id as CNT').from("t_rooms").where({isDeleted:null})
   res.json({total:total[0].CNT, skip:r.params.skip, rooms:r});
