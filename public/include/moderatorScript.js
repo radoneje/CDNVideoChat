13deb4d6dba678217d876a625644688b298babe0
radoneje <den.shevchenko@gmail.com>
2022-07-25 15:56:19 +0300

    e

--- public/include/moderatorScript.js
+++ public/include/moderatorScript.js
@@ -2,11 +2,30 @@
     const vue =new Vue({
         el:"#app",
         data:{
-            chatConfigShow:false
+            chatConfigShow:false,
+            chat:[],
+            chatText:"",
+            userError:null,
+            user:{id:null, name:null},
+            reqUserShow:false,
         },
-        methods:{},
+        methods:{
+            updateStatus:async function(){
+                try {
+                    let s = await axios.get("/api/status/" + this.id)
+                    this.status = s.data.status;
+                    this.chat=s.data.chat;
+                }
+                catch (e){console.warn(e)}
+                setTimeout(this.updateStatus, 2000);
+            }
+        },
         mounted:async function(){
-            console.log("vue")
+            let u=localStorage.getItem("user_"+this.id);
+            if(u)
+                this.user=JSON.parse(u);
+            this.isLoaded=true;
+            this.updateStatus();
         }
     })
 })();
