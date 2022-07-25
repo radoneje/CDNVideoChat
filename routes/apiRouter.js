d28c58c9a8b7b967b50175b6606a0d598ec0abc5
radoneje <den.shevchenko@gmail.com>
2022-07-26 00:49:14 +0300

    e

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -207,6 +207,7 @@ router.post("/chat", async (req, res)=>{
   if(room.length==0)
     return res.sendStatus(404);
   let r= await req.knex("t_chat").insert({ roomPublicUUID:req.body.id,  userid:req.body.userid, file:req.file.path, fileName:req.file.originalname, fileType:req.file.mimetype, fileSize:req.file.size},"*");
+  console.log(r[0]);
   let rr=await req.knex("v_chat").where({id:r[0].id});
   console.log(rr[0]);
   res.json(rr[0]);
