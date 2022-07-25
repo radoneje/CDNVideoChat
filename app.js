711906d51dad598440fa00792c2decf5be6f2ae0
radoneje <den.shevchenko@gmail.com>
2022-07-25 13:47:49 +0300

    e

--- app.js
+++ app.js
@@ -70,4 +70,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+
 module.exports = app;
