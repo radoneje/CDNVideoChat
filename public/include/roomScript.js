47b0b2c6a6c1e2c7daa1db95c1ad85be84f6c4b7
radoneje <den.shevchenko@gmail.com>
2022-07-26 13:48:06 +0300

    e

--- public/include/roomScript.js
+++ public/include/roomScript.js
@@ -120,7 +120,7 @@ const sRoom=class{
 
                     this.status = s.data.status;
                     this.timeout=Number.parseInt( s.data.timeout);
-                    console.log()
+                    console.log(s.data.votes)
                     this.votes=s.data.votes.filter(v=>v.isactive)
                     if(this.timeout<2 || this.timeout>120)
                         this.timeout==20;
