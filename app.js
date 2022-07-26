8e144fa07e4e5f55b20303b8b977bfdbe8fceaa1
radoneje <den.shevchenko@gmail.com>
2022-07-26 17:15:53 +0300

    e

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/in/include', express.static(path.join(__dirname, 'public', "include")));
 
 const pgSession = require('connect-pg-simple')(session);
 const pgStoreConfig = {conObject: config.pgConnection}
